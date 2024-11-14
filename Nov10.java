480620bb0de042632716019766078d2d7a2e8aea
HarshSahuu1234 <harshsahuu162@gmail.com>
2024-11-14 10:52:29 +0530

    Update Nov10.java
    MY OPINION --> 
    
    Question 27:  Remove element!!
    This question seems easy but is not, actually it's tricky. I will tell you why.
    
    QUESTION DESCRIPTION --> 
    
    As the name suggests, we have to remove a given element from a given array and return the length of the array after removing all the given element from it.
    
    APPROACH --> 
    
    Firstly, someone can think of creating a new temporary array and store those elements which are not equal to given element and return its length, but when you initialize the temporary array, you have to give its size which we don't know, and our code will fail because if you try to give the size of original array it will return the size of original array itself, and not the temporary array.
    
    So, what we can do is that to make changes in the original array itself by creating a pointer which will move upon the addition of an element (not equal to given element) to original array and another pointer which will move through the whole array, or we can say that by iterating through the array (using for loop).
    
    TIME COMPLEXITY --> O (n)
    SPACE COMPLEXITY --> O (1)

--- Nov10.java
+++ Nov10.java
@@ -1,11 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int[] expectedNums = new int[nums.length];
+        int elements = 0;
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i]!=val){
-                expectedNums[i] = nums[i];
+                nums[elements] = nums[i];
+                elements++;
             }
         }
-        return expectedNums.length;
+        return elements;
     }
 }
